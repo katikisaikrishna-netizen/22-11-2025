8e638cf15dc53a7f4407be5ca86834f471b5e386
Sai Krishna <katikisaikrishna@gmail.com>
2025-11-22 09:57:16 +0530

    My first react app

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import JsxDemo from './components/JsxDemo';
+
+function App() {
+  return (
+    <div className="App">
+     <JsxDemo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/components/JsxDemo.jsx
@@ -0,0 +1,9 @@
+function JsxDemo( ){
+    return(
+        <div>
+            <h1>JSX Example</h1>
+        </div>
+    )
+}
+
+export default JsxDemo;
